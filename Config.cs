0f484c94a5e3a1be53e97385343ad01812a7ba11
Adisol <62602034+Adisol07@users.noreply.github.com>
2024-09-18 07:37:14 +0200

    Default MaxReasoningIterations changed

--- Config.cs
+++ Config.cs
@@ -4,7 +4,7 @@ namespace ReasoningAI;
 
 public class Config
 {
-    public int MaxReasoningIterations { get; set; } = 5;
+    public int MaxReasoningIterations { get; set; } = 8;
     public string OllamaAddress { get; set; } = "http://localhost:11434/";
     public int Animation { get; set; } = 1;
     public int MaxAnimationMsPerToken { get; set; } = 21;
